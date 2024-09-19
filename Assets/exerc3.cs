ac99ac7d84ed1845c25d433a882af6e954d3948f
lucianatux <lucianatux@yahoo.com.ar>
2024-09-19 13:10:59 -0300

    exerc5

--- Assets/exerc3.cs
+++ Assets/exerc3.cs
@@ -17,28 +17,20 @@ string weekDayMethod(int weekDayNumber){
 {
 case 1:
 return("Lunes");
-break;
 case 2:
 return("Martes");
-break;
 case 3:
 return("Miércoles");
-break;
 case 4:
 return("Jueves");
-break;
 case 5:
 return("Viernes");
-break;
 case 6:
 return("Sábado");
-break;
 case 7:
 return("Domingo");
-break;
 default:
 return("Error: the number must be from 1 to 7");
-break;
 }
 
 }
--- Assets/exerc5.cs
+++ Assets/exerc5.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class exerc5 : MonoBehaviour
@@ -7,7 +9,11 @@ public class exerc5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i <500; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                Debug.Log(i);
+            }
+        }
     }
 
     // Update is called once per frame
