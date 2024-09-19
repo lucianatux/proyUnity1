a0fb68bf1e4c198036c938b98b68c972ed5782a7
lucianatux <lucianatux@yahoo.com.ar>
2024-09-19 12:52:47 -0300

    ex4

--- Assets/exerc4.cs
+++ Assets/exerc4.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class exerc4 : MonoBehaviour
 {
+    public int number;
+    public int quantity;
     // Start is called before the first frame update
     void Start()
     {
+        int counter = 0;
+        if(quantity > 0){
+while (counter < quantity){
+            Debug.Log(number);
+            counter++;
+        }
+        }else{
+            Debug.Log("Error. No quantity");
+        }
         
     }
 
