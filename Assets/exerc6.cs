fba0d6842e1079ef7c85f0c81ea26d79c88f3dc0
lucianatux <lucianatux@yahoo.com.ar>
2024-09-19 13:43:20 -0300

    exerc6

--- Assets/exerc6.cs
+++ Assets/exerc6.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class exerc6 : MonoBehaviour
 {
+    public int firstRangeParameter;
+    public int secondRangeParameter;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (firstRangeParameter <= secondRangeParameter)
+        {
+            for (int i = firstRangeParameter; i <= secondRangeParameter; i++)
+            {
+                Debug.Log(firstRangeParameter * i);
+            }
+        }
+        else
+        {
+            for (int i = secondRangeParameter; i <= firstRangeParameter; i++)
+            {
+                Debug.Log(secondRangeParameter * i);
+            }
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
